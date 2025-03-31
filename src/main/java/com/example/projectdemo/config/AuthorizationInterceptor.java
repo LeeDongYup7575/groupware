0b297c1fc96a4ba2481c73c92754bbeaf80381fb
MiR Lee <mirlee@MiRui-MacBookAir.local>
2025-03-31 11:51:13 +0900

    fix: 인증 개발모드 적용

--- src/main/java/com/example/projectdemo/config/AuthorizationInterceptor.java
+++ src/main/java/com/example/projectdemo/config/AuthorizationInterceptor.java
@@ -20,18 +20,18 @@ public class AuthorizationInterceptor implements HandlerInterceptor {
     @Override
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
         // JWT 필터에서 설정한 사용자 역할 가져오기
-        String role = (String) request.getAttribute("role");
-
-        // 요청 URI 확인
-        String requestURI = request.getRequestURI();
-
-        // 관리자 전용 API 확인
-        if (requestURI.startsWith("/api/admin") && !isAdmin(role)) {
-            response.setStatus(HttpServletResponse.SC_FORBIDDEN);
-            response.setContentType("application/json");
-            response.getWriter().write("{\"message\":\"관리자 권한이 필요합니다.\",\"status\":403}");
-            return false;
-        }
+//        String role = (String) request.getAttribute("role");
+//
+//        // 요청 URI 확인
+//        String requestURI = request.getRequestURI();
+//
+//        // 관리자 전용 API 확인
+//        if (requestURI.startsWith("/api/admin") && !isAdmin(role)) {
+//            response.setStatus(HttpServletResponse.SC_FORBIDDEN);
+//            response.setContentType("application/json");
+//            response.getWriter().write("{\"message\":\"관리자 권한이 필요합니다.\",\"status\":403}");
+//            return false;
+//        }
 
         return true;
     }
