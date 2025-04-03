7dfdaadf6e4f624e4dee6c3956f53ced2bda9c0f
PJH <ksoark0109@naver.com>
2025-04-03 17:37:52 +0900

    휴가 신청 및 근무현황 달력페이지 구현입니다

--- src/main/java/com/example/projectdemo/domain/leave/dao/LeavesDAO.java
+++ src/main/java/com/example/projectdemo/domain/leave/dao/LeavesDAO.java
@@ -1,4 +1,19 @@
 package com.example.projectdemo.domain.leave.dao;
 
+import com.example.projectdemo.domain.edsm.dto.EdsmBcDTO;
+import org.apache.ibatis.session.SqlSession;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+@Repository
 public class LeavesDAO {
+
+    @Autowired
+    private SqlSession mybatis;
+
+    public int insertByLeaves(EdsmBcDTO bcdto) {
+
+        return mybatis.insert("com.example.projectdemo.domain.leave.dao.LeavesDAO.insertByLeaves", bcdto);
+
+    }
 }
--- src/main/java/com/example/projectdemo/domain/leave/service/LeavesService.java
+++ src/main/java/com/example/projectdemo/domain/leave/service/LeavesService.java
@@ -1,4 +1,17 @@
 package com.example.projectdemo.domain.leave.service;
 
+import com.example.projectdemo.domain.edsm.dto.EdsmBcDTO;
+import com.example.projectdemo.domain.leave.dao.LeavesDAO;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
 public class LeavesService {
+
+    @Autowired
+    private LeavesDAO leavesDAO;
+
+    public int insertByLeaves(EdsmBcDTO bcdto) {
+        return leavesDAO.insertByLeaves(bcdto);
+    }
 }
