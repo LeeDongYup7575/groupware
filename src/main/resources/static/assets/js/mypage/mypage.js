7e5bf2365283461f806e36d2c4600bd60a3bad6e
Yunjin Kang <riveryj.kang@gmail.com>
2025-04-12 00:25:52 +0900

    Design: 마이페이지 보안 탭의 '마지막 로그인' 문구를 '마지막 활동'으로 수정

--- src/main/resources/static/assets/js/mypage/mypage.js
+++ src/main/resources/static/assets/js/mypage/mypage.js
@@ -168,7 +168,7 @@ function generateContent(contentName, data) {
     
     <div class="info-container">
         <div class="info-row">
-          <div class="info-label">마지막 로그인</div>
+          <div class="info-label">마지막 활동</div>
           <div class="info-content" id="lastLogin">${data.lastLogin}</div>
         </div>
         <div class="info-row">
