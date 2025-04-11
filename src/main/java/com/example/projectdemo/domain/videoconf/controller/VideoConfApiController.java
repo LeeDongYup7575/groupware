c4f89f8dcf73ddde1159cddf6299a28be3b726b1
MiR Lee <mirlee@MiRui-MacBookAir.local>
2025-04-11 10:48:13 +0900

    rename api controller

--- src/main/java/com/example/projectdemo/domain/videoconf/controller/VideoConfApiController.java
+++ src/main/java/com/example/projectdemo/domain/videoconf/controller/VideoConfApiController.java
@@ -14,7 +14,7 @@ import java.util.List;
 @RestController
 @RequestMapping("/api/videoconf")
 @RequiredArgsConstructor
-public class VideoConfRestController {
+public class VideoConfApiController {
 
     private final VideoConfService videoConfService;
 
